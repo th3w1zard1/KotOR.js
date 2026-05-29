bf5c042f31b8e872d2dd4504bf8aae5e2d112cbf
Boden <halomastar@gmail.com>
2026-05-29 03:30:47 -0500

    test(loading): add LoaderProgress unit tests
    Cover LoaderProgressTracker lifecycle and formatLoaderEta formatting.
    Configure ts-jest with jest types so tests compile under the root tsconfig.

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,11 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
-    "^.+.ts?$": ["ts-jest", {}],
+    "^.+.ts?$": ["ts-jest", {
+      tsconfig: {
+        types: ['jest', 'node'],
+      },
+    }],
   },
   testMatch: ['**/*.test.ts'],
   // Exclude Vitest-only tests that use adapters not in this repo (holocron port)
